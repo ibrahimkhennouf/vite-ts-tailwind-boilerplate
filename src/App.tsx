5dfff9a7c828091d1776f81929c671648b1c6ddd
Ibrahim <ibrahimkhennouf10@gmail.com>
2023-06-21 21:08:45 +0100

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">todo app</h1>
+      <h1 className="text-3xl font-bold text-center">
+        unleashe your creativity
+      </h1>
     </>
   );
 }
